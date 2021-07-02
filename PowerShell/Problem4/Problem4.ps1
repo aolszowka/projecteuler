1e355af8f1f5861a66d8e5f4b3658be220a6aee9
Ace Olszowka <aolszowka@gmail.com>
2021-07-02 13:08:50 -0600

    Minor Perf Improvement to use StringBuilder

--- PowerShell/Problem4/Problem4.ps1
+++ PowerShell/Problem4/Problem4.ps1
@@ -7,15 +7,16 @@ function Test-Palindrome {
     process {
         # Need a String.Reverse
         [System.Collections.Generic.Stack[char]]$stack = @()
-        foreach($char in $PossiblePalindrome.GetEnumerator()) {
+        foreach ($char in $PossiblePalindrome.GetEnumerator()) {
             $stack.Push($char)
         }
-        while($stack.Count -ne 0) {
-            $reversed = $reversed + $stack.Pop()
+        [System.Text.StringBuilder]$reversed = [System.Text.StringBuilder]::new()
+        while ($stack.Count -ne 0) {
+            $reversed.Append($stack.Pop()) | Out-Null
         }
 
         # See if the reversed is equal to the input
-        $PossiblePalindrome -eq $reversed
+        $PossiblePalindrome -eq $($reversed.ToString())
     }
 }
 
@@ -23,10 +24,10 @@ function Problem4-Solution {
     [System.Collections.Generic.LinkedList[int]]$palindromes = @()
 
     # Brute Force the Entire Space
-    for($i = 999; $i -gt 0; $i--) {
-        for($j = 999; $j -gt 0; $j--) {
+    for ($i = 999; $i -gt 0; $i--) {
+        for ($j = 999; $j -gt 0; $j--) {
             $product = $i * $j
-            if(Test-Palindrome $product) {
+            if (Test-Palindrome -PossiblePalindrome $product.ToString()) {
                 $palindromes.Add($product) | Out-Null
                 break
             }
